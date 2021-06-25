0126fc401af6ce7c784995430822502af8bc5ccb
Maneshwar <mehramaneshwar4@gmail.com>
2021-06-25 15:31:16 +0530

    Added some informative comments

--- whatsapp-frontend/src/components/Chat.js
+++ whatsapp-frontend/src/components/Chat.js
@@ -1,34 +1,34 @@
 import { Avatar, IconButton } from '@material-ui/core'
-import { AttachFile, MoreVert , SearchOutlined, InsertEmoticon, Mic} from '@material-ui/icons'
-import React,{ useEffect, useState } from 'react'
+import { AttachFile, MoreVert, SearchOutlined, InsertEmoticon, Mic } from '@material-ui/icons'
+import React, { useEffect, useState } from 'react'
 import './Chat.css'
 import axios from './axios'
 import { useStateValue } from './StateProvider'
 
-const Chat = ({messages}) => {
-    
-        const [seed, setSeed] = useState("")
-        const [input,setInput] = useState("")
-        const [{user}, dispatch] = useStateValue()
-        const sendMessage = async(e) =>{
-            e.preventDefault()
-            await axios.post('/messages/new',{
-                message: input,
-                name:user.displayName,
-                timestamp: new Date().toUTCString(),
-                received: true
-            } ,[])
-            setInput("")
-        }
+const Chat = ({ messages }) => {
+
+    const [seed, setSeed] = useState("") // to show random avatars
+    const [input, setInput] = useState("")
+    const [{ user }, dispatch] = useStateValue()
+    const sendMessage = async (e) => {
+        e.preventDefault()
+        await axios.post('/messages/new', {
+            message: input,
+            name: user.displayName,
+            timestamp: new Date().toUTCString(),
+            received: true
+        }, [])
+        setInput("")
+    }
     useEffect(() => {
-        setSeed(Math.floor(Math.random() * 5000))
-        
+        setSeed(Math.floor(Math.random() * 5000))  // to show random avatars
+
     }, [])
     return (
         <div className="chat">
             <div className="chat__header">
 
-                <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />        
+                <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
                 <div className="chat__headerInfo">
                     <h2>Dev1</h2>
                     <p>Last seen at {" "}
@@ -48,10 +48,10 @@ const Chat = ({messages}) => {
                 </div>
             </div>
             <div className="chat__body">
-                {messages.map(message =>(
-                    <p className={ `chat__message ${message.name === user.displayName && `chat__receiver`}`}>
+                {messages.map(message => (
+                    <p className={`chat__message ${message.name === user.displayName && `chat__receiver`}`}>
                         <span className="chat__name">{message.name}</span>
-                            {message.message}
+                        {message.message}
                         <span className="chat__timestamp">
                             {message.timestamp}
                         </span>
@@ -61,8 +61,8 @@ const Chat = ({messages}) => {
             <div className="chat__footer">
                 <InsertEmoticon />
                 <form>
-                    <input value={input} onChange={e => setInput(e.target.value)}
-                     type="text" placeholder= "Enter Message" />
+                    <input value={input} onChange={e => setInput(e.target.value)} /*control component*/
+                        type="text" placeholder="Enter Message" />
                     <button onClick={sendMessage} type="submit">Send</button>
                 </form>
                 <Mic />
--- whatsapp-frontend/src/components/SidebarChat.js
+++ whatsapp-frontend/src/components/SidebarChat.js
@@ -1,17 +1,17 @@
 import { Avatar } from '@material-ui/core'
 
-import React,{ useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './SidebarChat.css'
 
-function SidebarChat({messages}) {
-    const [seed, setSeed] = useState("")
+function SidebarChat({ messages }) {
+    const [seed, setSeed] = useState("") // to make it dynamic/random
     useEffect(() => {
-        setSeed(Math.floor(Math.random() * 5000))
-        
+        setSeed(Math.floor(Math.random() * 5000))  // to make it dynamic/random
+
     }, [])
     return (
         <div className="sidebarChat">
-            <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />        
+            <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
             <div className="sidebarChat__info">
                 <h2>Dev1</h2>
                 <p>{messages[messages.length - 1]?.message}</p>
